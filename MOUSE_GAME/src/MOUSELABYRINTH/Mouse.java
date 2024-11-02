a91dec163cf9f0e8e045a7343b34c749b6251b39
IHaydenm <aplex1405@gmail.com>
2024-11-01 22:48:46 -0600

    got the file data into an array

--- MOUSE_GAME/src/MOUSELABYRINTH/Mouse.java
+++ MOUSE_GAME/src/MOUSELABYRINTH/Mouse.java
@@ -1,6 +1,74 @@
 package MOUSELABYRINTH;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Mouse {
     public static void main(String[] args) throws Exception {
-        System.out.println(MouseFileOpener.FileOpening());
+        /*VARIABLES*/
+        int rows = 0;
+        int columns = 0;
+        String fileDataReading = " "; /*OPENING FILE WITH PARENT*/
+        File FileMapDir = new File("C:/Users/katco/Documents/les javas/MOUSE_GAME/src/MOUSELABYRINTH");/*OBTAINING THE INFORMATION COMING IN FORM SRC*/
+        String[] ArchNames = FileMapDir.list();
+        for(int i=0; i < ArchNames.length; i++){/*GIVING AN ARRAY THE NAMES FORM THE FILES INSIDE THE PARENT*/
+            System.out.println(i+1 + ". " + ArchNames[i]);/*LISTING FILES INSIDE REPOSITORY*/
+        } /*ERRASE AFTER TESTS */
+        System.out.println("ESTOS SON LOS ARCHIVOS\n\nPOR FAVOR ELIJE UN .TXT PARA TRABAJARLO COMO MAPA PARA EL RATON.\n\nTIENES QUE ESCRIBIR EL NUMERO DEL ARCHIVO QUE SE MUESTRA A LA IZQUIERDA DE SU NOMBRE.");
+        try (Scanner sc = new Scanner(System.in)) {/*MAKING THE USER SELECT THE FILE INSIDE THE PARENT DIRECTORY */
+            int op = sc.nextInt();
+                    File inFileMap = new File("C:/Users/katco/Documents/les javas/MOUSE_GAME/src/MOUSELABYRINTH/", ArchNames[op-1]);
+                    if(inFileMap.exists()){/*SPECIFIED FILE EXISTS CONDITION */
+                        System.out.println("EL ARCHIVO EXISTE");
+                        /*FILE READER FOR VARIABLE BUFREAD*/
+                        FileReader ReadingFile = new FileReader(inFileMap);
+                        BufferedReader bufRead = new BufferedReader(ReadingFile); 
+                        
+                        String readFile = " ";
+                        for(int i=0; i < inFileMap.length();i++){
+                            if(readFile==null){
+                                break;
+                            }
+                            else{
+                                readFile = bufRead.readLine();
+                                System.out.println(readFile);
+                                rows = rows + 1;
+                            }         
+                        }
+                        FileReader tempReaderForColumns = new FileReader(inFileMap);
+                        try (BufferedReader temBufferedForColumns = new BufferedReader(tempReaderForColumns)) {
+                            String tempColumnsQuantity = temBufferedForColumns.readLine();
+                            columns = tempColumnsQuantity.length();
+                        }
+                    }
+                        else{/*SPECIFIED FILE DOES NOT EXIST */
+                            System.out.println("EL ARCHIVO NO EXISTE.");
+                         }
+                        int tRows = rows - 1;
+                        System.out.println(tRows + " " + columns);
+                        /*THIS CONVERTS DATA FORM THE FILE TO STRING*/
+                        /*FILE READER FOR VARIABLE BUFREADPRE1*/
+                        FileReader ReadingFilePre1 = new FileReader(inFileMap);
+                        BufferedReader bufReadPrel = new BufferedReader(ReadingFilePre1);
+                        List <String> numbers = new ArrayList <String>(); 
+                        int limit = 0;
+                        System.out.println("The string from the file: ");
+                        while(limit<tRows){
+                            String tempLineReader = " ";
+                                tempLineReader = bufReadPrel.readLine().toString();
+                            
+                                numbers.add(tempLineReader);
+                                fileDataReading = numbers.get(limit);
+                                System.out.println(fileDataReading);
+                                limit++;
+                            }
+                            MouseLabyrinth myLabyrinth = new MouseLabyrinth(rows, columns);
+                            System.out.println(myLabyrinth.MousePathfinding(numbers));
+                }
+                
     }
 }
--- MOUSE_GAME/src/MOUSELABYRINTH/MouseLabyrinth.java
+++ MOUSE_GAME/src/MOUSELABYRINTH/MouseLabyrinth.java
@@ -1,14 +1,67 @@
 package MOUSELABYRINTH;
+
+import java.util.List;
+
 public class MouseLabyrinth {
-    int[][] aviableMap;
+    boolean hasFoundS = false;
+    /*THESE VARIABLES ARE SET TO MARK THE SPAWNPOINT*/
+    int spR = 0; 
+    int spC = 0;
+    /*THESE VARIABLES ARE SET TO MARK THE SPAWNPOINT*/
+    /*THESE VARIABLES ARE THE PRESETS FOR THE MAP CONDITIONS*/
+    char for0 = '0';
+    char for1 = '1';
+    char forF = 'F';
+    char[][] aviableMap;
+    char[] gettingChar;
     int rows, columns;
     public MouseLabyrinth(int rows, int columns){
         this.rows = rows;
         this.columns = columns;
-        this.aviableMap = new int[rows][columns];
+        this.aviableMap = new char[rows][columns];
     }
-    public String MousePathfinding(){
-        
+    public String MousePathfinding(List <String> map){
+        /*PLACING THE DATA FROM THE FILE INSIDE THE LOCAL VARIABLE AVIABLEMAP */
+        int mapLenght = map.size();
+        System.out.println("THIS IS THE MAP LENGHT FROM MOUSE LABYRINTH: " + mapLenght);
+        gettingChar = new char[mapLenght];
+        for(int i=0;i<rows-1;i++){
+            gettingChar = map.get(i).toCharArray();/*THIS WILL GET THE STRING FROM MAP AND CONVERT THE DATA INTO CHARS*/
+            for(int j=0;j<columns;j++){
+                aviableMap[i][j] = gettingChar[j];
+                System.out.print(gettingChar[j]);
+            }
+            System.out.print("\n");
+        }/*PREVIOUS SECTION WILL CREATE THE MAP THAT WILL BE ANALIZED*/
+        /*THIS SET OF THE CODE WILL FIND THE BEGINING OF THE MAP */
+        for(int i=0;i<rows-1;i++){
+            if(hasFoundS==true){
+                break;
+            }
+                for(int j=0;j<columns;j++){
+                    if(hasFoundS==true){
+                        break;
+                    }
+                    else if(aviableMap[i][j]!='S'){
+                        System.out.println("ESTOY BUSCANDO EL PUNTO DE PARTIDA...");
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else{
+                        System.out.println("ESTAS SON LAS COORDENADAS DE COMIENZO"+ "(" + i + ") (" + j + ")");
+                        hasFoundS = true;
+                        spC = j;
+                        spR = i;
+                    }
+                }
+            }/*MAP PATHFINDER MAP PATHFINDER MAP PATHFINDER MAP PATHFINDER MAP PATHFINDER MAP PATHFINDER MAP PATHFINDER */
+            
+            if(aviableMap[spR][spC]==for0){
+                
+            }
         return " ";
     }
 }
