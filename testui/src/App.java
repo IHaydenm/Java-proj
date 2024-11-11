446c8a207f458b1f67da644f39cae15fb39f9c68
IHaydenm <aplex1405@gmail.com>
2024-11-11 11:55:37 -0600

    testing swing@

---
+++ testui/src/App.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+
+import javax.swing.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame window = new JFrame();
+        window.setSize(400 , 400 );
+
+        window.getContentPane().setBackground(Color.BLACK);
+        window.setVisible(true);
+    }
+}
